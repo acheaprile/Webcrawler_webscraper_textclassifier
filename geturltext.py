05609a3097b81d6fd8883aea6642a073f1b88cfd
acheaprile <danielgarciache@gmail.com>
2020-08-28 16:25:40 +0200

    Add existing file

--- geturltext.py
+++ geturltext.py
@@ -1,35 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import time
-from time import gmtime
-from time import strftime
 
-def gettext(urls):
+#Extract the text (if any) from a set of links
+def gettext(urls, textlength):
     
     finaltexts=pd.DataFrame()
     
     for u in list(urls.values):
         
         try:
+            #Get the website
             res=requests.get(u[1])
+            #Get the content of the server’s response
             html=res.text
-            soup = BeautifulSoup(html, features="html.parser")
-            document=[e.text.replace("\n"," ") for e in soup.findAll("p")]
-            if len(" ".join(document))>2000:
-                document=pd.Series(" ".join(document[int(len(document)*0.02):int(len(document)*0.9)]))
+            #Parsing the HTML
+            parsed = BeautifulSoup(html, features="html.parser")
+            #Replace the line breaks in the paragraphs
+            document=[e.text.replace("\n"," ") for e in parsed.findAll("p")]
+            #Extract the text from the url
+            if len(" ".join(document))>textlength:
+                #We join the paragraphs in the page
+                document=pd.Series(" ".join(document))
                 url=pd.Series(str(u[1]))
                 finaltexts=pd.concat([finaltexts,pd.DataFrame({"text":document, "url":url}).dropna()])
         except:
             pass
+    #We remove any text duplicates keeping the first unique url
     finaltexts=finaltexts.drop_duplicates(subset ="text")
     
     return finaltexts
 
 if __name__== "__main__":
     
-    start_time = time.time()
-    file=str(input("File name: "))
+    file, textlength=str(input("File name: ")),int(input("Minimum number of text char. required:"))
     urls=pd.read_csv("{}.csv".format(file))
-    gettext(urls).to_excel("urltexts.xlsx", encoding='utf-8-sig')
-    print ("My program took", strftime("%H:%M:%S", gmtime(time.time() - start_time)), "to run")
+    gettext(urls, textlength).to_excel("urltexts.xlsx", encoding='utf-8-sig')
--- maincrwl.py
+++ maincrwl.py
@@ -5,38 +5,42 @@ from urllib.parse import urljoin
 import urllib.robotparser
 import pandas as pd
 import tldextract
-import time
-from time import gmtime
-from time import strftime
 
-start_time = time.time()
-
-
-#Connecting to the site
+#Sends a request to get a website and its response
 def web(urladress):
     
     try:
+        #We format the url passed to always include http in the url
         if 'http' not in urladress:
             web.site = 'http://{}'.format(urladress)
         elif 'http' in urladress[:4]:
             web.site=str(urladress)   
+        
+        #Get the website
         web.res=requests.get(web.site)
+        
+        #Get the content of the server’s response
         web.html=web.res.text
         
     except:
+        #If the server's response is bad, we skip that url
         if web.res.status_code!=200:
-            print ("*****Connection error with site {}, look for error code {}".format(urladress,str(web.res.status_code)))
+            print ("*****Connection error with site {}, look for error code {}".format(urladress,
+                   str(web.res.status_code)))
             pass
         else:
             pass
     
     return(web.html,web.site)
 
-#Parsing the html
+#Parsing the HTML
 def bf(html):
     
     bf.soup = BeautifulSoup(html, features="html.parser")
+    
+    #Gets all the links within the parsed url
     bf.tags=[a["href"] for a in bf.soup.find_all(href=True)]
+    
     return (bf.soup,bf.tags)
  
 #Extracting all urls in the page
@@ -45,31 +49,40 @@ def geturls(tags):
     geturls.allurls=[]
     append=geturls.allurls.append
     
-    #Defining which urls within the site are scrappable
+    #Checking which urls within the domain are scrappable by checking the robots.txt file
     rp = urllib.robotparser.RobotFileParser()
     rp.set_url(urljoin(web(url)[1],"robots.txt"))
     rp.read()
     
-    #Extracting all the urls in the main page
+    #Extracting all the urls in the link if it has http in the url, the robots.txt file allows us to and
+    # if the link belongs to the parent domain
     for tag in tags:
  
-        if 'http' in tag[:4] and tldextract.extract(tag)[1]==tldextract.extract(url)[1] and str(rp.can_fetch("*",tag))=="True":
-            append(tag)
-        elif 'http' not in tag[:4] and url in tag and tldextract.extract('http://'+tag)[1]==tldextract.extract(url)[1] and str(rp.can_fetch("*",'http://'+tag))=="True":
-            append('http://{}'.format(tag))
-        elif 'http' not in tag[:4] and url not in tag and tldextract.extract('http://'+url+tag)[1]==tldextract.extract(url)[1] and str(rp.can_fetch("*",'http://'+url+tag))=="True":
-            append('http://{}{}'.format(url,tag))
+        if 'http' in tag[:4] and tldextract.extract(tag)[1]==tldextract.extract(url)[1]\
+                    and str(rp.can_fetch("*",tag))=="True":
+                        append(tag)
+                        
+        elif 'http' not in tag[:4]\
+                    and url in tag and tldextract.extract('http://'+tag)[1]==tldextract.extract(url)[1]\
+                    and str(rp.can_fetch("*",'http://'+tag))=="True":
+                        append('http://{}'.format(tag))
+                        
+        elif 'http' not in tag[:4]\
+                    and url not in tag and tldextract.extract('http://'+url+tag)[1]==tldextract.extract(url)[1]\
+                    and str(rp.can_fetch("*",'http://'+url+tag))=="True":
+                        append('http://{}{}'.format(url,tag))
      
     return (geturls.allurls)
 
 
+#Extracting iteratively all the links within all the links that have been pulled previously
 def allurls(mainurls):
 
     allurls=set([subsite.lower() for site in mainurls for subsite in geturls(bf(web(site)[0])[1])])
       
     return (set(allurls))
         
-# Calling the functions and exporting the output to an excel
+# Calling all the functions and exporting the output to an excel
 if __name__== "__main__":
     
     parents=pd.read_csv("{}\parentsites.csv".format(os.getcwd()),header=None)
@@ -82,5 +95,3 @@ if __name__== "__main__":
         urlsoutput=pd.concat([urlsoutput,pd.DataFrame(websiteurls)])
     
     urlsoutput.to_csv("mainoutput.csv")
-    
-    print ("My program took", strftime("%H:%M:%S", gmtime(time.time() - start_time)), "to run")
